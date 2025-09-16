f3fcbc44e1c7dea6abea8567e51ec59b01a79855
Zaka Elab <zakaelab@connectedsoma.com>
2025-09-16 16:57:23 +0200

    Add random_state and stratification

--- src/model.py
+++ src/model.py
@@ -9,20 +9,27 @@ from mlflow.models import infer_signature
 
 from xgboost import XGBClassifier
 
-df = pd.read_pickle(os.path.join('.', 'data', 'processed', 'dataset.pkl'))
+# SEEDS
+RANDOM_STATE = 42
 
-y = df['class']
-X = df[[col for col in df.columns if col != 'class']]
+df = pd.read_pickle(os.path.join(".", "data", "processed", "dataset.pkl"))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+y = df["class"]
+X = df[[col for col in df.columns if col != "class"]]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=RANDOM_STATE,
+    stratify=y
+)
 
 # Define the model hyperparameters
 params = {
-    'n_estimators' : 2,
-    'max_depth' : 2, 
-    'learning_rate' : 1, 
-    'objective' : 'binary:logistic',
-    'eval_metric' : 'logloss',
+    'n_estimators': 2,
+    'max_depth': 2,
+    'learning_rate': 1,
+    'random_state' : RANDOM_STATE,
+    'objective': 'binary:logistic',
+    'eval_metric': 'logloss',
 }
 
 bst = XGBClassifier(**params)
@@ -37,7 +44,7 @@ recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 
 # Configure MLFlow
-mlflow.set_tracking_uri(uri='http://127.0.0.1:8082')
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8082")
 
 # Create a new MLflow Experiment
 mlflow.set_experiment("Adult income classifier")
@@ -49,9 +56,9 @@ with mlflow.start_run():
 
     # Log the loss metrics
     mlflow.log_metric("accuracy", accuracy)
-    mlflow.log_metric('precision', precision)
-    mlflow.log_metric('recall', recall)
-    mlflow.log_metric('f1', f1)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1", f1)
 
     # Infer the model signature
     signature = infer_signature(X_train, bst.predict(X_train))
@@ -67,5 +74,6 @@ with mlflow.start_run():
 
     # Set a tag that we can use to remind ourselves what this model was for
     mlflow.set_logged_model_tags(
-        model_info.model_id, {"Training Info": "Basic XGBoost classifier for adult income"}
+        model_info.model_id,
+        {"Training Info": "Basic XGBoost classifier for adult income"},
     )
